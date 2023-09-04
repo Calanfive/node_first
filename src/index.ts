300a9a18c874e185af1e554d680a9efcb1419b2a
calanfive <camille.lelan@gmail.com>
2023-09-04 16:47:13 +0200

    objet_crea_interface

--- src/index.ts
+++ src/index.ts
@@ -10,4 +10,24 @@ if (posName !== -1) {
     console.log("Bonjour ", process.argv[posName + 1]);
 }
 
+// nom, prenom, age, sexe
+
+interface Ipersonne { 
+    nom: string;
+    prenom: string;
+    age: number;
+    sexe: string;
+}
+
+const pers : Ipersonne = {
+    prenom: "cam",
+    nom: "pam",
+    age: 50,
+    sexe: "h",
+}
+
+const newName = {...pers, nom:"laforge", prenom:"thomas"}
+
+const newName2 = {...pers, nom:"amish", prenom:"fatir"}
+console.log(pers, newName, newName2);
 
